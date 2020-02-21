21ed84a8cb25cba6ebd94c1835908441f6cfffa1
Raphael Jakobs <raphy@airmail.cc>
2020-02-21 20:03:53 +0100

    Imported the whole skeleton of the website into django.

---
+++ backend/home/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+##CHI_SIAMO
+def contatti(request):
+    return render(request, "chi_siamo/contatti.html")
+
+def orari(request):
+    return render(request, "chi_siamo/orari.html")
+
+def organigramma(request):
+    return render(request, "chi_siamo/organigramma.html")
+
+
+##SERVIZI
+def iscrizione(request):
+    return render(request, "servizi/iscrizione.html")
+
+##PROGETTI
+def cyberbullismo(request):
+    return render(request, "progetti/cyberbullismo.html")
+
+def PCTO(request):
+    return render(request, "progetti/PCTO.html")
+
+def PON(request):
+    return render(request, "progetti/PON.html")
