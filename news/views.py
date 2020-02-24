9b78cb74661a8bc7847467e92580a6d62d0f2a71
Raphael Jakobs <raphy@airmail.cc>
2020-02-24 23:05:31 +0100

    adding rudimental pagination, adding news-specific page

--- news/views.py
+++ news/views.py
@@ -9,9 +9,11 @@ from django.views import generic
 from .models import Post
 
 class PostList(generic.ListView):
+    paginate_by = 5
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'notizie.html'
 
+
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'notizia.html'
