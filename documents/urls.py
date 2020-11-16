f6eeafff2e2eefd4276b05511a32f536c0a72600
Raphael Jakobs <raphy@airmail.cc>
2020-11-16 21:35:28 +0100

    search bar modulistica

--- documents/urls.py
+++ documents/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.DocList.as_view(), name="modulistica"),
-    path('search', views.SearchResult.as_view())
+    path('', views.SearchResult.as_view(), name="modulistica"),
+    path('search', views.SearchResult.as_view(), name='search')
 ]
 
--- documents/views.py
+++ documents/views.py
@@ -4,9 +4,19 @@ from .models import Document
 # Create your views here.
 
 class DocList(generic.ListView):
-    paginate_by = 5
+    paginate_by = 50
     queryset = Document.objects.order_by('-created_on')
     template_name = 'documents.html'
 
+
 class SearchResult(generic.ListView):
+    paginate_by = 20
+    queryset = Document.objects.order_by('-created_on')
     template_name = 'documents.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query is None:
+            return Document.objects.order_by('-created_on')
+        else:
+            return Document.objects.filter(name__icontains=query)
